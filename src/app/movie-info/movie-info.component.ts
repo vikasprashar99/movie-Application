44ee953a52aa50ea0d0869aa83aade9abeca7bd7
vikas <vikasprashar99@gmail.com>
2020-02-09 14:58:17 +0530

    added last component

--- src/app/movie-info/movie-info.component.ts
+++ src/app/movie-info/movie-info.component.ts
@@ -1,21 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
+import { NgbRatingConfig, NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+import { MovieserviceService } from '../movieservice.service';
 
+
+export class MovieDetails{
+  Sno:number;
+  MovieName:string;
+  StarRating:string;
+  Year:number;
+  isFeatured:boolean;
+}
 @Component({
   selector: 'app-movie-info',
   templateUrl: './movie-info.component.html',
   styleUrls: ['./movie-info.component.css']
 })
 export class MovieInfoComponent implements OnInit {
-  data: number[] = [3, 4, 1];
-
-  constructor(private router:Router,config: NgbRatingConfig) {
+ 
+  a=[  {
+    Sno:66,
+    MovieName:"Incredibals 2",
+    StarRating:"5",
+    Year:2019,
+    isFeatured:false
+  },
+  {
+    Sno:73,
+    MovieName:"Captain Marvel",
+    StarRating:"4",
+    Year:2019,
+    isFeatured:false
+  },
+  {
+    Sno:64,
+    MovieName:"Frozen  ",
+    StarRating:"5",
+    Year:2013,
+    isFeatured:false
+  },
+  {
+    Sno:14,
+    MovieName:"Harry Potter and the Chamber of Secrets  ",
+    StarRating:"1",
+    Year:2001,
+    isFeatured:true
+  },
+  {
+    Sno:93,
+    MovieName:"Harry Potter and the Deathly Hallows - Part 1  ",
+    StarRating:"4",
+    Year:2010,
+    isFeatured:false
+  },
+  {
+    Sno:11,
+    MovieName:"Harry Potter and the Goblet of Fire  ",
+    StarRating:"3",
+    Year:2020,
+    isFeatured:false
+  }
+  ]
+  Movienamevar;
+  Ratingsvar;
+  constructor(private router:Router,config: NgbRatingConfig, private Movieservice: MovieserviceService) {
     config.max = 5;
-    config.readonly = true;
    }
 
   ngOnInit() {
+this.Movienamevar=this.Movieservice.moviename
+this.Ratingsvar=this.Movieservice.Rating;
+    console.log(this.Movieservice.moviename,this.Movieservice.Rating)
   }
   back(){
     this.router.navigate(['/Movies'])
@@ -23,4 +78,6 @@ export class MovieInfoComponent implements OnInit {
   Logout(){
     this.router.navigate(['/Signup'])
   }
+
+  
 }
--- src/app/movieslist/movieslist.component.ts
+++ src/app/movieslist/movieslist.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { NgbRatingConfig } from "@ng-bootstrap/ng-bootstrap";
 import { StarRatingModule } from "angular-star-rating";
 import { Router } from "@angular/router";
+import { MovieserviceService } from '../movieservice.service';
 
 export class MovieDetails{
   Sno:number;
@@ -18,9 +19,9 @@ export class MovieDetails{
   providers: [NgbRatingConfig] // add NgbRatingConfig to the component providers
 })
 export class MovieslistComponent implements OnInit {
-<<<<<<< HEAD
 Movies:string[]=[];
 Moviesvar:MovieDetails[]=[];
+currentRate=[];
 a=[  {
   Sno:66,
   MovieName:"Incredibals 2",
@@ -65,11 +66,12 @@ a=[  {
 }
 ]
 
-constructor(private router:Router,config: NgbRatingConfig) {
-  config.max = 5;
-  config.readonly = true;
- }
 
+constructor(private router:Router,config: NgbRatingConfig, private Movieservice: MovieserviceService,
+  ) {
+  config.max = 5;
+
+ }
    stars: number[] = [1, 2, 3, 4, 5];
    selectedValue: number;
    
@@ -85,82 +87,28 @@ constructor(private router:Router,config: NgbRatingConfig) {
       ob.StarRating=this.a[e].StarRating;
       ob.isFeatured=this.a[e].isFeatured;
       this.Moviesvar.push(ob)
+      this.currentRate.push(ob.StarRating)
      }
-     console.log(this.Moviesvar)
+     console.log(this.currentRate)
   }
   
   loadmoreMovies(n){
     console.log(n)
     this.a.push()
-=======
-  a = [
-    {
-      Sno: 66,
-      MovieName: "Incredibals 2",
-      StarRating: "5",
-      Year: "2019",
-      isFeatured: false
-    },
-    {
-      Sno: 73,
-      MovieName: "Captain Marvel",
-      StarRating: "4",
-      Year: "2019",
-      isFeatured: false
-    },
-    {
-      Sno: 64,
-      MovieName: "Frozen  ",
-      StarRating: "5",
-      Year: "2013",
-      isFeatured: false
-    },
-    {
-      Sno: 14,
-      MovieName: "Harry Potter and the Chamber of Secrets  ",
-      StarRating: "1",
-      Year: "2001",
-      isFeatured: true
-    },
-    {
-      Sno: 93,
-      MovieName: "Harry Potter and the Deathly Hallows - Part 1  ",
-      StarRating: "4",
-      Year: "2010",
-      isFeatured: false
-    },
-    {
-      Sno: 11,
-      MovieName: "Harry Potter and the Goblet of Fire  ",
-      StarRating: "3",
-      Year: "2005",
-      isFeatured: false
-    }
-  ];
-
-  itemsLimit = 6;
-  constructor(config: NgbRatingConfig, private router: Router) {
-    config.max = 5;
-    config.readonly = true;
-  }
-
-  ngOnInit() {}
-  loadmoreMovies(n) {
-    console.log(n);
-    this.a.push();
->>>>>>> 1d69dc6adc2ef6a57783f99c714062bbf5ab715f
   }
   Logout() {
     this.router.navigate(["/Signup"]);
   }
-  movieInfo() {
+  movieInfo(data) {
+    console.log(data)
+    this.Movieservice.moviename=data.MovieName;
+    this.Movieservice.Rating=data.StarRating;
     this.router.navigate(["/Movieinfo"]);
   }
-  countStar(star) {
-    console.log('Value of star', star);
-  }
+ 
   SortByName(){
     return this.Moviesvar.sort((a, b) => (a.MovieName < b.MovieName ? -1 : 1))
+    
   }
   SortByYear(){
   return this.Moviesvar.sort((a, b) => (a.Year > b.Year ? -1 : 1))
